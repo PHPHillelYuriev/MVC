b884f949afbfdd9110e4ef3082f25a658affbba8
Yuriev Dmitriy <yurievdmitriy1989@gmail.com>
2018-04-24 18:25:22 +0300

    View.php was modified

--- app/Core/View.php
+++ app/Core/View.php
@@ -3,12 +3,26 @@ class View
 {
     public function render($template, $data = null)
     {
-        $content = '../src/views/' . $template . '.php';        
+        $content = $this->getTemplatePath($template);        
 
-        $layout = '../src/views/layout.php';
+        $layout = $this->getLayoutPath();
 
         if (file_exists($layout)) {
             include_once($layout);
+        }
+
+        if (file_exists($content)) {
+            include_once($content);
         }        
+    }
+
+    public function getTemplatePath($template)
+    {
+        return ROOT . '/../src/views/' . $template . '.php';
+    }
+
+    public function getLayoutPath()
+    {
+        return ROOT . '/../src/views/layout.php';
     }
 }
